2e64cd9f985a8051105174da380a1ab4c2d57b9b
Maroooo <rado.dayef@gmail.com>
2026-02-23 12:58:26 +0200

    Full Video Code

--- bin/main.dart
+++ bin/main.dart
@@ -1,1 +1,27 @@
-void main() {}
+void main() {
+  num number = -4.7;
+  int integerNumber = 5;
+  double doubleNumber = 1.2;
+
+  print(number); // Output: -4.7
+  print(doubleNumber); // Output: 1.2
+  print(integerNumber); // Output: 5
+
+  // Convert to double
+  print(number.toDouble()); // Output: -4.7
+
+  // Convert to string
+  print(number.toString()); // Output: "-4.7"
+
+  // Get absolute value (removes negative sign)
+  print(number.abs()); // Output: 4.7
+
+  // Round to the nearest integer
+  print(number.round()); // Output: -5
+
+  // Round up to the nearest higher integer
+  print(number.ceil()); // Output: -4
+
+  // Round down to the nearest lower integer
+  print(number.floor());// Output: -5
+}
